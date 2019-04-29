872fd333165fd498041640fe9e694f9a9efc78d0
enyk99 <enyk99@gmail.com>
2019-04-29 17:06:41 -0500

    Se sube a master el juego corregido

---
+++ Assets/Scripts/AparecerYDesaparecer.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class AparecerYDesaparecer : MonoBehaviour
+{
+
+    public void Aparecer_Desaparecer(GameObject OnjetoADesaparecer)
+    {
+        if (OnjetoADesaparecer.activeSelf == true)
+        {
+            OnjetoADesaparecer.SetActive(false);
+            Debug.Log("Activado");
+        }
+        else
+        {
+            OnjetoADesaparecer.SetActive(true);
+            Debug.Log("Desactivar");
+        }
+    }
+}
---
+++ Assets/Scripts/TestMostrarHighScore.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestMostrarHighScore : MonoBehaviour {
+
+    void Start ()
+    {
+        Debug.Log(PlayerPrefs.GetFloat("ScoreEasy"));
+
+        Debug.Log(PlayerPrefs.GetFloat("ScoreNormal"));
+
+        Debug.Log(PlayerPrefs.GetFloat("ScoreHard"));
+
+        Debug.Log(PlayerPrefs.GetFloat("ScoreExpert"));
+
+    }
+	
+}
